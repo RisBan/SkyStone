23bd831880a724854c8ee81452dce12efb83a2cb
RisBan <43934791+RisBan@users.noreply.github.com>
2019-12-07 14:32:20 -0500

    Starting Hardware

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Hardware {
+
+    //motors
+    public DcMotor frontRight = null;
+    public DcMotor backRight = null;
+    public DcMotor frontLeft = null;
+    public DcMotor backLeft = null;
+
+    //webcam
+
+}
